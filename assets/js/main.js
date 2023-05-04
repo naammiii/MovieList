8b13600d9691164503840878a4bb26a8b82fda20
Iman Boukaich Talai <tribtt@gmv.com>
2023-05-04 11:56:08 +0200

    top carousels done. need fix style

--- assets/js/main.js
+++ assets/js/main.js
@@ -68,6 +68,26 @@ scrollLinks.forEach((scrollLink) => {
 	
 });
 
+/* ======= Carousel animation ======= */ 
+$('.carousel .carousel-item').each(function(){
+    var minPerSlide = 4;
+    var next = $(this).next();
+    if (!next.length) {
+    next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+    
+    for (var i=0;i<minPerSlide;i++) {
+        next=next.next();
+        if (!next.length) {
+        	next = $(this).siblings(':first');
+      	}
+        
+        next.children(':first-child').clone().appendTo($(this));
+      }
+});
+
+
 /* ===== Gumshoe SrollSpy ===== */
 /* Ref: https://github.com/cferdinandi/gumshoe  */
 
