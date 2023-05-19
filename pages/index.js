675e236668f7be7931cd3c04dcdea772b653e4f1
Iman BT <imanbt11@gmail.com>
2023-05-19 15:50:14 +0200

    adden login signup and routing

--- pages/index.js
+++ pages/index.js
@@ -12,77 +12,7 @@ function Home() {
   useEffect(() => {
     import('bootstrap/dist/js/bootstrap');
   }, []);
-  useEffect(() => {
-    "use strict";
-
-    const linkSty = styles.a;
-
-/* ======= Header animation ======= */   
-const header = document.getElementById('header');  
-
-window.onload=function() 
-{   
-    headerAnimation(); 
-
-};
-
-window.onresize=function() 
-{   
-    headerAnimation(); 
-
-}; 
-
-window.onscroll=function() 
-{ 
-    headerAnimation(); 
-
-}; 
-    
-
-function headerAnimation () {
-
-    var scrollTop = window.scrollY;
-	
-	if ( scrollTop > 0 ) {	    
-	    header.classList.add('header-scrolled');    
-	    	    
-	} else {
-	    header.classList.remove('header-scrolled');
-	}
-
-};
-
-let scrollLinks = document.querySelectorAll('.scrollto');
-const pageNavWrapper = document.getElementById('navbar-collapse');
-
-scrollLinks.forEach((scrollLink) => {
-
-	scrollLink.addEventListener('click', (e) => {
-		
-		e.preventDefault();
-
-		let element = document.querySelector(scrollLink.getAttribute("href"));
-		
-		const yOffset = 60; //page .header height
-		
-		//console.log(yOffset);
-		
-		const y = element.getBoundingClientRect().top + window.pageYOffset - yOffset;
-		
-		window.scrollTo({top: y, behavior: 'smooth'});
-		
-		
-		//Collapse mobile menu after clicking
-		if (pageNavWrapper.classList.contains('show')){
-			pageNavWrapper.classList.remove('show');
-		}
-
-		
-    });
-	
-});
-
-  })
+  
   return (
     <div className={styles.container}>
       <Head>
