6584a24e10a39a626a01bbe3995a71f4c0525ad1
Iman BT <imanbt11@gmail.com>
2023-05-19 10:51:40 +0200

    鶏肉

---
+++ views/assets/js/details.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const LittleApp = () => (
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+  );
+  
+  ReactDOM.render(<LittleApp />, document.getElementById("littleApp"));
+  
--- web-scrapping.py
+++ web-scrapping.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
 HEADERS ={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0", "Accept-Encoding":"gzip, deflate", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1", 'Accept-Language': 'en-US,en;q=0.5'}
 URL = "https://www.imdb.com/title/tt4154796/"
@@ -21,4 +22,9 @@ for genre in aux.find_all_next('span', attrs= {'class': 'ipc-chip__text'}):
 
 quote['genres'].remove('Back to top')
 
-print(quote['genres'])
+json_string = json.dumps(quote)
+
+file = open('data.json', "w")
+file.write(json_string)
+file.close()
+
