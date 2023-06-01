5d66bbd4e9fac0aeae6d2ef9ef55cf5ea25e9a16
Iman BT <imanbt11@gmail.com>
2023-06-01 08:32:02 +0200

    a ver si funcina

--- pages/api/get/getUsers.ts
+++ pages/api/get/getUsers.ts
@@ -2,6 +2,5 @@ import prisma from '../../../lib/prisma';
 
 export default async function getUsers(req, res) {
     const allUsers = await prisma.user.findMany()
-    console.log(allUsers)
     res.json(allUsers);
 }
