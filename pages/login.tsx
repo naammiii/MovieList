674028a3101a416edeeb535010992d0d462261de
Iman BT <imanbt11@gmail.com>
2023-06-01 08:36:02 +0200

    ahah

--- pages/login.tsx
+++ pages/login.tsx
@@ -85,7 +85,7 @@ export default function Login ()
                 if(data) {
                     Router.push('/home');
                 }else{
-                    Router.push('/');
+                    window.location.reload();
                 }
             });
         };
